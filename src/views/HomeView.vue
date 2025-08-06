7c046042796bb4cd93d97e0dc9d2688938bdf9a2
Thomas Farrell <>
2025-08-06 17:09:29 -0400

    Latest on homepage slider

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -70,9 +70,10 @@ onMounted(() => {
       },
       '>'
     )
-    .to('.bg-gradient p', {
+    .to(bgGradient.value, {
       y: -100,
-      opacity: 0
+      opacity: 0,
+
     })
 })
 
@@ -80,7 +81,7 @@ onUnmounted(() => {
 
   const video = document.querySelector('.bg-video')
   if (video) {
-    video.playbackRate = 2.0 // 🔁 change to desired speed (e.g., 2.0 for 2x)
+    video.playbackRate = 3.0 // 🔁 change to desired speed (e.g., 2.0 for 2x)
   }
 
   ScrollTrigger.getAll().forEach(trigger => trigger.kill())
@@ -91,65 +92,80 @@ onUnmounted(() => {
 
 
 <template>
-    <section class="hero-home" ref="hero">
-      <video class="bg-video" src="@/assets/img/1037517047-preview.mp4" loop muted autoplay></video>
+  <section class="hero-home" ref="hero">
+    <video class="bg-video" src="@/assets/img/1037517047-preview.mp4" loop muted autoplay></video>
 
-      <svg class="txt-hero" ref="txtHero">
-        <mask id="mask">
-          <rect fill="white" width="100%" height="100%"></rect>
-          <text id="engage" dominant-baseline="central" x="50%" y="30%" text-anchor="middle">engage</text>
-          <text id="inspire" dominant-baseline="central" x="50%" y="30%" text-anchor="middle">inspire</text>
-          <text id="react" dominant-baseline="central" x="50%" y="30%" text-anchor="middle">react</text>
-        </mask>
+    <svg class="txt-hero" ref="txtHero">
+      <mask id="mask">
+        <rect fill="white" width="100%" height="100%"></rect>
+        <text id="engage" dominant-baseline="central" x="50%" y="30%" text-anchor="middle">engage</text>
+        <text id="inspire" dominant-baseline="central" x="50%" y="30%" text-anchor="middle">inspire</text>
+        <text id="delight" dominant-baseline="central" x="50%" y="30%" text-anchor="middle">delight</text>
+      </mask>
 
-        <rect width="100%" height="100%" id="mask-bg"></rect>
-      </svg>
+      <rect width="100%" height="100%" id="mask-bg"></rect>
+    </svg>
 
-      <div class="bg-gradient" ref="bgGradient">
-        <video class="other-video" autoplay muted loop>
-          <source src="@/assets/img/1037517047-preview.mp4" type="video/mp4">
-        </video>
-        <div class="gradient-text">
-          <p>Design is not just my profession; it's my passion, my very essence. I live and breathe design, and it permeates every aspect of my life. My mind constantly wanders, exploring new ideas, concepts, and possibilities. I find myself unable to turn my brain off, always seeking inspiration from the world around me. As an award-winning designer, I've had the privilege of working for top agencies across New York, where I've crafted everything from sleek websites to captivating campaigns to all immersive experiential events, aiming to engage, inspire, and react.</p>
-          <p class="callout">Design isn't just what I do—it's who I am, and I pour my heart and soul into every project I undertake.</p>
-        </div>
-      </div>
-    </section>
-    <section class="project-slider">
+    <div class="bg-gradient" ref="bgGradient">
+      <video class="other-video" autoplay muted loop>
+        <source src="@/assets/img/1037517047-preview.mp4" type="video/mp4">
+      </video>
       <div class="container">
-        <div class="slider-wrap">
-         
-          <span class="txt-passion">Passion</span>
-        
-          <Carousel v-bind="carouselConfig">
-             <Slide>
-              <div class="slide">
-                
-                <p class="text-1">Handcrafted leather, die cuts, debossing—this Pristine Water brochure has all the bells & whistles. Oh, and it won a Graphic Design USA Award!</p>
-                <img src="@/assets/img/home-elit-brochure-desktop-2x.png" alt="" class="slide-1-img">
-                <p class="text-2">A showcase of my most impactful and unforgettable design and project accomplishments amassed throughout my career.</p>
-                
-              </div>
-            </Slide>
-            <Slide>
-              <div class="slide">
-                
-                <p class="text-1">Sleek websites, scroll-stopping socials, and digital campaigns that truly delivered results.</p>
-                <img src="@/assets/img/home-versace-desktop-2x.png" alt="" class="slide-2-img">
-                <p class="text-2">A showcase of my most impactful and unforgettable design and project accomplishments amassed throughout my career.</p>
-                
-              </div>
-            </Slide>
-          
-            <template #addons>
-              <Pagination />
-            </template>
-          </Carousel>
-
-          <span class="txt-projects">Projects</span>
-        </div>
+        <p>Design is not just my profession; it's my passion, my very essence. I live and breathe design, and it
+          permeates every aspect of my life. My mind constantly wanders, exploring new ideas, concepts, and
+          possibilities. I find myself unable to turn my brain off, always seeking inspiration from the world around me.
+          As an award-winning designer, I've had the privilege of working for top agencies across New York, where I've
+          crafted everything from sleek websites to captivating campaigns to all immersive experiential events, aiming
+          to engage, inspire, and react.</p>
+        <p class="callout">Design isn't just what I do—it's who I am, and I pour my heart and soul into every project I
+          undertake.</p>
+      </div>
     </div>
-    </section>
+  </section>
+  <section class="project-slider">
+    <div class="container">
+      <div class="slider-wrap">
+
+        <span class="txt-passion">Passion</span>
+
+        <Carousel v-bind="carouselConfig">
+          <Slide>
+            <div class="slide">
+              <div class="text-1">
+                <img src="@/assets/img/logo-elit-2x.png" alt="" class="slide-logo-elit">
+                <p>Handcrafted leather, die cuts, debossing—this Pristine Water brochure has all the
+                  bells & whistles. <strong>Oh, and it won a Graphic Design USA Award!</strong></p>
+              </div>
+              <img src="@/assets/img/home-elit-brochure-desktop-2x.png" alt="" class="slide-img">
+              <div class="text-2 with-plus">
+                <p><strong>Featured Work</strong></p>
+                <p>A showcase of my most impactful and unforgettable design and project accomplishments
+                amassed throughout my career.</p>
+              </div>
+
+            </div>
+          </Slide>
+          <Slide>
+            <div class="slide">
+
+              <p class="text-1">Sleek websites, scroll-stopping socials, and digital campaigns that truly delivered
+                results.</p>
+              <img src="@/assets/img/home-versace-desktop-2x.png" alt="" class="slide-img">
+              <p class="text-2">A showcase of my most impactful and unforgettable design and project accomplishments
+                amassed throughout my career.</p>
+
+            </div>
+          </Slide>
+
+          <template #addons>
+            <Pagination />
+          </template>
+        </Carousel>
+
+        <span class="txt-projects">Projects</span>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style lang="scss">
@@ -159,6 +175,7 @@ onUnmounted(() => {
   height: 100vh;
   position: relative;
   overflow: hidden;
+
   .txt-hero {
     position: absolute;
     top: 0;
@@ -166,42 +183,43 @@ onUnmounted(() => {
     height: 100vh;
     width: 100vw;
   }
-}
 
+  #inspire,
+  #engage,
+  #delight {
+    font-family: "DM Serif Display", serif;
+    font-size: 180.23px;
+  }
 
-#inspire, #engage, #react {
-  font-family: "DM Serif Display", serif;
-  font-size: 180.23px;
-}
+  #inspire {
+    transform: translateY(140px)translateX(140px);
+  }
 
-#inspire {
-  transform: translateY(140px)translateX(140px);
-}
+  #delight {
+    transform: translateY(275px)translateX(-60px);
+  }
 
-#react {
-  transform: translateY(240px)translateX(-117px);
-}
+  .bg-video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100%;
+    min-height: 100%;
+    z-index: -100;
+  }
 
-.bg-video {
-  position: absolute;
-  top: 0;
-  left: 0;
-  min-width: 100%;
-  min-height: 100%;
-  z-index: -100;
-}
+  #mask-bg {
+    mask: url("#mask");
+  }
 
-#mask-bg {
-  mask:url("#mask");
-}
-
-.other-video {
-  position: absolute;
-  top: 0;
-  left: 0;
-  min-width: 100%;
-  min-height: 100%;
-  z-index: -100;
+  .other-video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100%;
+    min-height: 100%;
+    z-index: -100;
+  }
 }
 
 .bg-gradient {
@@ -214,95 +232,120 @@ onUnmounted(() => {
   align-items: center;
   justify-content: center;
 
+  .container {
+    max-width: 900px;
+  }
 
-  p, h3 {
+  p,
+  h3 {
     color: $white;
     max-width: 1300px;
     margin: 0 auto;
   }
+
   .callout {
     @extend h3;
   }
+
   p {
     margin-bottom: 20px;
   }
-  
+
 }
 
 .slider-wrap {
-  border: 2px solid red;
+  // border: 2px solid red;
   position: relative;
 
   .carousel {
-   position: relative;
-  overflow: hidden;
-  padding: 4rem 2rem;
-  min-height: 60vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-   z-index: 1;
-   img {
-    max-width: 70vw;
-    border: 1px solid red;
-    width: 100%;
-    height: auto;
-    object-fit: contain;
-   }
+    position: relative;
+    overflow: hidden;
+    padding: 5rem 2rem 0 2rem;
+    min-height: 60vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+
+    .slide-img {
+      max-width: 70vw;
+      // border: 1px solid red;
+      width: 100%;
+      height: auto;
+      object-fit: contain;
+    }
   }
+
   .carousel__slide {
     align-items: start;
   }
+ 
+  .carousel__pagination-button {
+    background-color: $lightCoolGray;
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+  }
+
+  .carousel__pagination-button--active {
+    background-color: $purple;
+  }
+
+  
+
   .text-1 {
     position: absolute;
-    top: 20vw;
+    top: 7vw;
     left: 0;
     width: 20vw;
   }
+
   .text-2 {
     position: absolute;
     top: 5vw;
     right: 0;
     width: 25vw;
+    &.with-plus {
+      &:before {
+        content: '+';
+        font-size: 20px;
+        color: $purple;
+        position: absolute;
+        left: 0;
+      }
+      padding-left: 20px;
+    }
   }
+
+  .slide-logo-elit {
+    width: 65px;
+    margin-bottom: 20px;
+  }
+
 }
 
-  .txt-passion, .txt-projects {
-    position: absolute;
-    // top: 50%;
-    // transform: translateY(-50%);
-    font-size: clamp(5rem, 10vw, 12rem);
-    font-weight: bold;
-    color: #3f3de6;
-    pointer-events: none;
-    user-select: none;
-  }
+.txt-passion,
+.txt-projects {
+  position: absolute;
+  font-family: $fontHeadline;
+  font-size: clamp(10rem, 10vw, 12rem);
+  font-weight: bold;
+  color: #3f3de6;
+  pointer-events: none;
+  user-select: none;
+}
 
-  .txt-passion { top: 0; left: 2%; z-index: 1; }
-  .txt-projects { bottom: 18%; right: 2%; z-index: 3; }
+.txt-passion {
+  top: 0;
+  left: 2%;
+  z-index: 1;
+}
 
+.txt-projects {
+  bottom: 25%;
+  right: 5%;
+  z-index: 3;
+}
 
-  // .slide-1-img {
-  //   position: absolute;
-  //   margin: 0 auto;
-  //   top: 50%;
-  //   transform: translateY(-50%);
-  //   left: 0;
-  //   right: 0;
-  //   width: 100%;
-  //   max-width: 800px;
-  //   z-index: 1;
-  // }
-  // .slide-2-img {
-  //   position: absolute;
-  //   margin: 0 auto;
-  //   top: 50%;
-  //   transform: translateY(-50%);
-  //   left: 0;
-  //   right: 0;
-  //   width: 100%;
-  //   max-width: 800px;
-  //   z-index: 1;
-  // }
 
 </style>
