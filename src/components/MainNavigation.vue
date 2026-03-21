ce1670095c727a78aa8777a59c87680053f90021
Thomas Farrell <>
2026-03-20 20:46:14 -0400

    Testing new styling for mobile button

--- src/components/MainNavigation.vue
+++ src/components/MainNavigation.vue
@@ -397,14 +397,14 @@ watch(isMenuOpen, (isOpen) => {
 
 .mobile-hamburger-container {
   position: fixed;
-  bottom: 0;
+  bottom: -2px;
   z-index: 10000;
   width: 100%;
   display: none;
 
   .hamburger-text {
     font-size: 12px;
-    color: $black;
+    color: $white;
     text-transform: lowercase;
     font-weight: 700;
     text-align: center;
@@ -479,13 +479,13 @@ watch(isMenuOpen, (isOpen) => {
 
   @media (max-width: $breakpoint-sm) {
     display: block;
-    // background: linear-gradient(to top, $white 0%, rgba($white, 0) 100%);
+    background: linear-gradient(to top, $black 0%, rgba($white, 0) 100%);
     /* Create a blur gradient effect instead of a solid color fade */
-    // backdrop-filter: blur(5px);
-    // -webkit-backdrop-filter: blur(5px);
-    // mask-image: linear-gradient(to top, black 40%, transparent 100%);
-    // -webkit-mask-image: linear-gradient(to top, black 40%, transparent 100%);
-    padding: 80px 0 15px; /* Increased top padding for smoother fade area */
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    mask-image: linear-gradient(to top, black 40%, transparent 100%);
+    -webkit-mask-image: linear-gradient(to top, black 40%, transparent 100%);
+    padding: 60px 0 17px; /* Increased top padding for smoother fade area */
     pointer-events: none;
 
     &.is-open {
