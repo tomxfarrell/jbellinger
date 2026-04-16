77be56702c5fefcfe5c16bcf7985eb00392ef82f
Tom <>
2026-04-16 08:39:00 -0400

    Latest

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     ViteImageOptimizer({
       png: { quality: 80 },
       jpeg: { quality: 75 },
